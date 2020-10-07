ca7bcec9aee1bcb087e46c0b5e4ea5852e3498be
FacuLavtech <facuhlavagninodq@gmail.com>
2020-10-06 21:31:31 -0300

    Form created

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,18 +4,19 @@ import './form.css';
 export default function Form() {
     return (
         <div className="form-card">
+            <div className="card-header">
+                <h1>Cotizador de criptomonedas</h1>
+            </div>
             <form>
-                <div className="form-header">
-                    <h1>Cotizador de criptomonedas</h1>
-                </div>
-                <div className="form-body">
-                    <select name="cripto" id="cripto">
-                        <option value="">Seleccione una criptomoneda</option>
-                    </select>
-                    <select name="fiat" id="fiat">
-                        <option value="">Seleccione una moneda</option>
-                    </select>
-                </div>
+                <label>Criptomoneda</label>
+                <select name="cripto" id="cripto">
+                    <option value="">Seleccione una criptomoneda</option>
+                </select>
+                <label>Moneda Fiat</label>
+                <select name="fiat" id="fiat">
+                    <option value="">Seleccione una moneda</option>
+                </select>
+                <input type="submit" name="cotizar" className="cotizar" value="Cotizar"/>
             </form>
         </div>
     )
